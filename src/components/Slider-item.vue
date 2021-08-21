c676ac70a36bca3176d9a59969e1f7f789029df9
VasiliyNefedov <nefedov.vasiliy@gmail.com>
2021-08-21 23:47:58 +0500

    feature slider 0.1

---
+++ src/components/Slider-item.vue
@@ -0,0 +1,76 @@
+<template>
+  <transition :name="direction" mode="in-out">
+    <div v-show="visibleSlide === index" class="carousel-item">
+      <slot></slot>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "Slider-item",
+  props: ['visibleSlide', 'index', 'direction']
+}
+</script>
+
+<style scoped>
+.carousel-item {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+}
+
+.left-enter-active {
+  animation: leftInAnimation 2.5s ease-in-out;
+}
+
+.left-leave-active {
+  animation: leftOutAnimation 2.5s ease-in-out;
+}
+
+@keyframes leftInAnimation {
+  from {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+
+@keyframes leftOutAnimation {
+  from {
+    transform: translateX(0);
+ }
+  to {
+    transform: translateX(-100%);
+  }
+}
+
+.right-enter-active {
+  animation: rightInAnimation 2.5s ease-in-out;
+}
+
+.right-leave-active {
+  animation: rightOutAnimation 2.5s ease-in-out;
+}
+
+@keyframes rightInAnimation {
+  from {
+    transform: translateX(-100%);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+
+@keyframes rightOutAnimation {
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(100%);
+  }
+}
+</style>
---
+++ src/components/Slider.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="carousel">
+    <slot></slot>
+    <button class="next" @click="next">next</button>
+    <button class="prev" @click="prev">prev</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Slider",
+  components: {},
+  data() {
+    return {
+      pics: [{src: '@/assets/slider/pic1.jpg'}, {src: '@/assets/slider/pic2.jpg'}, {src: '@/assets/slider/pic3.jpg'}],
+      src1: "@/assets/slider/pic1.jpg"
+    }
+  },
+  methods: {
+    next() {
+      this.$emit('next')
+    },
+    prev() {
+      this.$emit('prev')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.carousel {
+  position: relative;
+  width: 100%;
+  height: 300px;
+  overflow: hidden;
+}
+button {
+  position: absolute;
+  height: 40px;
+  width: 50px;
+  top: calc(50% - 20px);
+}
+button:focus, button:hover {
+  cursor: pointer;
+  outline: none;
+}
+.next {
+  right: 0;
+}
+.prev {
+  left: 0;
+}
+</style>
